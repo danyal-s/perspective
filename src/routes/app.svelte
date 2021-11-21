e2f16b58134dd55db95383c41f7491a9acad8d57
Danyal Soomro <danyal.s.94@gmail.com>
2021-11-21 21:40:21 +0500

    Moving to svelte kit

--- src/routes/app.svelte
+++ src/routes/app.svelte
@@ -4,8 +4,7 @@
 	export let name;
 	let schedulers = [];
 	let binance_assets = null;
-	let pancake_lp = null;
-	let all_assets = null;
+	let page_selected = "home";
 
 	async function get_binance_data () {
 		var res = await fetch("api/binance_assets");
@@ -15,7 +14,7 @@
 
 	onMount(async () => {
 		get_binance_data();
-		schedulers.append(setInterval(() => {get_binance_data()}, 5000));
+		schedulers.push(setInterval(() => {get_binance_data()}, 5000));
 		
 	});
 
@@ -23,50 +22,26 @@
 		clearInterval(element);
 	}));
 
-	
+	function change_page(page_name) {
+		page_selected = page_name
+	}
 
 </script>
 	<nav class="navbar navbar-expand-md navbar-dark bg-dark">
 		<a class="navbar-brand" href="#">Crypto App</a>
         <ul class="navbar-nav w-100">
-            <li class="nav-item"><a href="#" class="nav-link">Home</a></li>
-            <li class="nav-item"><a href="#" class="nav-link">Link</a></li>
+            <li class="nav-item"><button id="haha" href="#" on:click={change_page} class="nav-link">Home</button></li>
+            <li class="nav-item"><button href="#" on:click={change_page} class="nav-link">Link</button></li>
+			<li class="nav-item"><a href="javascript:void(0);" on:click={() => {page_selected = "hi"}} class="nav-link">Link</a></li>
+			<li class="nav-item"><button on:click={change_page} class="nav-link">Link</button></li>
+			<li class="nav-item"><button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"></button></li>
             <!--<li class="nav-item ml-md-auto"><a href="#" class="btn btn-primary switch">Switch Light/Dark</a></li>-->
         </ul>
 	</nav>
 	<section class="container">
+		<p>{page_selected}</p>
 	<div class="row">
 		<div class="col-md-12">
-			<h2>All Assets 
-				{#if all_assets != null}
-				(${all_assets.total_usd.toFixed(2)})
-				{/if}
-
-			</h2>
-			<table class="table table-dark">
-				<th>Symbol</th>
-				<th>Amount</th>
-				<th>USD Value of Token</th>
-				<th>USD Value Held</th>
-					{#if all_assets != null}
-						{#each Object.entries(all_assets.token_stats) as [token_name, token_stats]}
-						<tr>
-							<td>{token_name}</td>
-							<td>{token_stats.count}</td>
-							<td>${token_stats.current_price}</td>
-							<td>${(token_stats.count * token_stats.current_price).toFixed(2)}</td>
-						</tr>
-						{/each}
-					{:else}
-						<tr>
-							<td>Loading...</td>
-						</tr>
-					{/if}
-			</table>
-		</div>
-	</div>
-	<div class="row">
-		<div class="col-md-4">
 			<h2>Binance Info</h2>
 			<table class="table table-dark">
 				<th>Symbol</th>
